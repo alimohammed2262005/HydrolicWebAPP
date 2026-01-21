d1d5f1be311f03c1f61251773ff5ef0a335b935d
Ali Mohammed <alimohammed62005@gmail.com>
2026-01-21 21:29:31 +0200

    Update admin-project-images.ts

--- src/app/Components/admin-project-images/admin-project-images.ts
+++ src/app/Components/admin-project-images/admin-project-images.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Projectinterface } from '../../Interfaces/projectinterface';
 import { Environment } from '../../Environment/environment';
 import { ProjectServices } from '../../Services/project-services';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Spinner } from "../spinner/spinner";
 import { CommonModule } from '@angular/common';
+import { Roles } from '../../Services/roles';
+import { ProjectImagesStatus } from '../../Services/SubComponents/project-images-status';
+import { ProjectImagesService } from '../../Services/project-images-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-admin-project-images',
@@ -12,20 +16,37 @@ import { CommonModule } from '@angular/common';
   templateUrl: './admin-project-images.html',
   styleUrls: ['./admin-project-images.css'],
 })
-export class AdminProjectImages {
+export class AdminProjectImages implements OnInit, OnDestroy {
   loading: boolean = false;
   projects: Projectinterface[] = [];
+  hasDeletedImages = false;
   apiMessage: string = '';
   apiMessageType: 'success' | 'error' = 'success';
   environment = Environment.StaticFiles;
+  private deletedImagesSubscription?: Subscription;
 
   constructor(
     private projectService: ProjectServices,
-    private router: Router
+    private router: Router,
+    private projectImagesService: ProjectImagesService,
+    private routing: ActivatedRoute,
+    private projectImagesStatus: ProjectImagesStatus,
+    private roles: Roles,
   ) {}
 
   ngOnInit(): void {
     this.getAllProjects();
+    this.checkDeletedImages();
+    
+    this.deletedImagesSubscription = this.projectImagesStatus.deletedprojectImages$.subscribe(deletedImages => {
+      this.hasDeletedImages = deletedImages.length > 0;
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.deletedImagesSubscription) {
+      this.deletedImagesSubscription.unsubscribe();
+    }
   }
 
   getAllProjects() {
@@ -42,12 +63,37 @@ export class AdminProjectImages {
     });
   }
 
+  checkDeletedImages() {
+    this.projectImagesService.getAllDeletedProjectImages().subscribe({
+      next: deleted => {
+        this.projectImagesStatus.setDeletedProjectsImages(deleted);
+      },
+      error: () => {
+        this.projectImagesStatus.setDeletedProjectsImages([]);
+      }
+    });
+  }
+
   viewProjectImages(projectId: number) {
     this.router.navigate(['/projectimages', projectId]);
   }
+
   addImage() {
     this.router.navigate(['addprojectimage']);
   }
+
+  restoreDeletedImages() {
+    this.router.navigate(['getdeletedprojectimages']);
+  }
+
+  get isAdmin() {
+    return this.roles.isAdmin();
+  }
+
+  get isAuth() {
+    return this.roles.isAuthenticated();
+  }
+
   private showMessage(message: string, type: 'success' | 'error') {
     this.apiMessage = message;
     this.apiMessageType = type;
